c023df79e84a437caa2f4ef9cb5e1ef74103d218
Zach Gulde <zachgulde@gmail.com>
2016-07-11 15:38:15 -0500

    lock down create update and delete routes

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,6 +10,11 @@ use App\Post;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
