d2cc1e32c1c31b4e8d772e470864949c6aec1072
Zach Gulde <zachgulde@gmail.com>
2016-07-11 12:37:32 -0500

    add edit and delete buttons to post index page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,6 +10,19 @@
             <a href="{{ $post->link }}">
                 {{ $post->title }}
             </a>
+            <div class="pull-right">
+                <a class="btn btn-warning" href="{{ action('PostsController@edit', $post->id) }}">Edit</a>
+                <form style="display:inline" action="{{ action('PostsController@destroy', $post->id) }}" method="POST">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <input type="hidden" name="_method" value="DELETE">
+                    <input class="btn btn-danger" type="submit" value="Delete">
+                </form>
+            </div>
         </h2>
+        <p>
+            Posted: {{ $post->created_at->diffForHumans() }} | 
+            By: $user
+        </p>
     @endforeach
+    {!! $posts->links() !!}
 @stop
