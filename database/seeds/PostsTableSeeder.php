a55c162c4d30796a90c0cdca01564f135b4ce25d
Zach Gulde <zachgulde@gmail.com>
2016-07-11 20:50:52 -0500

    add user relationship to post model

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -11,8 +11,8 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Post::class, 50)->create()->each(function ($post) {
-            $post->save();
-        });
+        /* factory(App\Post::class, 50)->create()->each(function ($post) { */
+        /*     $post->save(); */
+        /* }); */
     }
 }
