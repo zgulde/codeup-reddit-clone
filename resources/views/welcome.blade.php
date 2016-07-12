364aef35893790729c59573e3304531ce7bdea3e
Zach Gulde <zachgulde@gmail.com>
2016-07-12 09:35:42 -0500

    add useful links to landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
 
 @section('content')
     @if(Auth::check())
-        <h1>Welcome, {{ Auth::user()->name }}.</h1>
-        <p><a href="/auth/logout">Logout</a></p>
+        <h1>Welcome, {{ Auth::user()->name }}.<p class="pull-right small"><a href="/auth/logout">Logout</a></p></h1>
+        <p><a href="{{ action('PostsController@create') }}">Create a New Post</a> | <a href="{{ action('PostsController@index') }}">View All Posts</a></p>
     @else
         <h1><a href="/auth/login">Login</a> | <a href="/auth/register">Register</a></h1>
     @endif
